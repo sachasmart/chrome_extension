57124cada60aa0e0c802459dd5f09752528812b7
Sacha Smart <sacha.smart@gmail.com>
2022-01-18 11:07:53 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-import { apiKey } from './apiKey.js'
-import { geoLocateKey } from './apiKey.js'
+import {
+	apiKey,
+	geoLocateKey,
+	TWILIO_ACCOUNT_SID,
+	TWILIO_AUTH_TOKEN,
+	TWILIO_PHONE_NUMBER,
+	mongodb_gas_price,
+	gas_price_api_key,
+} from './apiKey.js'
 
-// document.getElementById('origin-input').placeholder = 'Testing'
 let geoLocateUrl = `https://www.googleapis.com/geolocation/v1/geolocate?key=${geoLocateKey}`
-
-fetch(geoLocateUrl, { method: 'POST' })
-	.then((response) => response.json())
-	.then((data) => console.log(data))
 
 document.getElementById('form-el').addEventListener('submit', function (event) {
 	event.preventDefault()
+	document.getElementById('results').style.display = 'block'
+
 	const destinationInput = document.getElementById('destination-input').value
 	const originInput = document.getElementById('origin-input').value
 	const timeCost = document.getElementById('timeCost-input').value
@@ -19,6 +23,9 @@ document.getElementById('form-el').addEventListener('submit', function (event) {
 	let urlCompose =
 		'https://maps.googleapis.com/maps/api/distancematrix/json?' +
 		`destinations=${destinationInput}&origins=${originInput}&key=${apiKey}`
+	// //Gas Price API
+
+	//Google Maps Fetch
 	fetch(urlCompose)
 		.then((response) => response.json())
 		.then((data) => {
@@ -55,11 +62,43 @@ document.getElementById('form-el').addEventListener('submit', function (event) {
 			document.getElementById(
 				'timeCost-li'
 			).textContent = `Cost of Time: $${totalTimeCost.toFixed(2)}`
+		})
+	// Fetch Google Maps directions
+	let destinationInputEncoded = encodeURIComponent(destinationInput)
+	let originInputEncoded = encodeURIComponent(originInput)
+	let directionsUrl = `https://www.google.com/maps/dir/?api=1&origin=${originInputEncoded}&destination=${destinationInputEncoded}&travelmode=driving`
+	document
+		.getElementById('sendDirections')
+		.addEventListener('click', function (sendText) {
+			sendText.preventDefault
+			let sendingNumber = document.getElementById('phoneNumber').value
+			console.log(sendingNumber)
+			// Twilio Post
+			let myHeaders = new Headers()
+			myHeaders.append(
+				'Authorization',
+				'Basic QUM1NjE1YTliODBjYTg2N2RiMTNmZTBjOGE5NDhiMjJlYToyYTQzNTI5ODJhZWIwNzBmODQzMTE5ZDU4MDdjNWY3ZA=='
+			)
+			myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
 
-			document.getElementById(
-				'additionalResource'
-			).innerHTML = `<style="border-top: 1px solid black"><a target="_blank" href="https://terrapass.com/product/productindividuals-families">Purchase Carbon-offsets for your <u>${distanceValue.toFixed(
-				0
-			)}km trip</u> to ${location}</style>`
+			let urlencoded = new URLSearchParams()
+			urlencoded.append('To', sendingNumber)
+			urlencoded.append('Body', `📍Google Maps Directions: ${directionsUrl}`)
+			urlencoded.append('From', '+19378836682\n')
+
+			let requestOptions = {
+				method: 'POST',
+				headers: myHeaders,
+				body: urlencoded,
+				redirect: 'follow',
+			}
+
+			fetch(
+				'https://api.twilio.com/2010-04-01/Accounts/AC5615a9b80ca867db13fe0c8a948b22ea/Messages.json',
+				requestOptions
+			)
+				.then((response) => response.text())
+				.then((result) => console.log(result))
+				.catch((error) => console.log('error', error))
 		})
 })
